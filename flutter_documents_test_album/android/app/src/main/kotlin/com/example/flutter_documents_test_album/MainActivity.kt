8d3fb4b63d58aae49d6203034638a1c02e772916
mubozk <mubozkt@gmail.com>
2022-09-18 07:13:27 +0300

    commit

---
+++ flutter_documents_test_album/android/app/src/main/kotlin/com/example/flutter_documents_test_album/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_documents_test_album
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flutter_test_from_docs/android/app/src/main/kotlin/com/example/flutter_test_from_docs/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_test_from_docs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
